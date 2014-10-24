4cc4042f2e87085785b1994dea2e5c97a2c18139
JMEsteve <jestevesoler@gmail.com>
2014-10-24 12:42:07 +0200

    Prueba de JS
    Subida fuentes prueba JS

---
+++ datos.js
@@ -0,0 +1,70 @@
+{"Familia":
+[
+{"Persona":
+	[
+		{"id":1, "Nombre":"Juan Manuel Esteve Soler","F.Nac.":1978, "F.Dec":null},
+		{"id":2, "Nombre":"Vicente Jose Esteve Soler","F.Nac.":02/01/1966, "F.Dec":null},
+		{"id":3, "Nombre":"Antonio Esteve Soler","F.Nac.":26/02/1967, "F.Dec":31/12/2004},
+		{"id":4, "Nombre":"Jose Esteve Olmos","F.Nac.":03/04/1938, "F.Dec":25/11/2005},
+		{"id":5, "Nombre":"Manuela Soler Blasco","F.Nac.":23/08/1940, "F.Dec":null},
+		{"id":6, "Nombre":"Susana Raya Zapata","F.Nac.":11/01/1978, "F.Dec":null},
+		{"id":7, "Nombre":"Antoni Esteve Raya","F.Nac.":13/02/2012, "F.Dec":null},
+		{"id":8, "Nombre":"Javier Esteve Raya","F.Nac.":04/05/2014, "F.Dec":null},
+		{"id":9, "Nombre":"Borja Esteve Molner","F.Nac.":4/12/1998, "F.Dec":null},
+		{"id":10, "Nombre":"Mar Esteve Molner","F.Nac.":19/11/2005, "F.Dec":null},
+		{"id":11, "Nombre":"Brigida Blasco Blasco","F.Nac.":1911, "F.Dec":14/08/1997},
+		{"id":12, "Nombre":"Vicente Soler","F.Nac.":1905, "F.Dec":14/08/1963},
+		{"id":13, "Nombre":"Concepcion Olmos Igual","F.Nac.":1909, "F.Dec":20/04/2002},
+		{"id":14, "Nombre":"Jose Esteve Angel","F.Nac.":1908, "F.Dec":13/11/1976},
+		{"id":15, "Nombre":"Amparo Molner Herraez","F.Nac.":1966, "F.Dec":null},
+		{"id":16, "Nombre":"Jose Esteve Blat","F.Nac.":"desc", "F.Dec":"desc"},
+		{"id":17, "Nombre":"Francisca Angel Martinez","F.Nac.":"desc", "F.Dec":"desc"},
+		{"id":18, "Nombre":"Francisco Olmos Taberner","F.Nac.":"desc", "F.Dec":11/06/1955},	SEGUNDA ENSANCHE 0196 4	
+		{"id":19, "Nombre":"Encarnacion Igual Miralles","F.Nac.":"desc", "F.Dec":03/07/1936} SEGUNDA ENSANCHE 0196 4
+
+   	]
+},
+
+{"Tipo Relacion":
+	[
+		{"id":1, "Nombre":"Paternidad"},
+		{"id":2, "Nombre":"Matrimonio"}
+	]
+},
+
+
+{"Relacion":
+	[
+		{"P1":4, "P2":1, "TR":1, "Inicio":null "Fin":null},
+		{"P1":4, "P2":2, "TR":1, "Inicio":null "Fin":null},		
+		{"P1":4, "P2":3, "TR":1, "Inicio":null "Fin":null},
+		{"P1":5, "P2":1, "TR":1, "Inicio":null "Fin":null},
+		{"P1":5, "P2":2, "TR":1, "Inicio":null "Fin":null},		
+		{"P1":5, "P2":3, "TR":1, "Inicio":null "Fin":null},
+		{"P1":4, "P2":5, "TR":2, "Inicio":1965 "Fin":null},
+		{"P1":1, "P2":6, "TR":2, "Inicio":2010 "Fin":null},
+		{"P1":1, "P2":7, "TR":1, "Inicio":null "Fin":null},
+		{"P1":1, "P2":8, "TR":1, "Inicio":null "Fin":null},
+		{"P1":6, "P2":7, "TR":1, "Inicio":null "Fin":null},
+		{"P1":6, "P2":8, "TR":1, "Inicio":null "Fin":null},
+		{"P1":2, "P2":9, "TR":1, "Inicio":null "Fin":null},
+		{"P1":2, "P2":10, "TR":1, "Inicio":null "Fin":null},
+		{"P1":15, "P2":9, "TR":1, "Inicio":null "Fin":null},
+		{"P1":15, "P2":10, "TR":1, "Inicio":null "Fin":null},
+		{"P1":2, "P2":15, "TR":2, "Inicio":1993 "Fin":null},
+		{"P1":11, "P2":5, "TR":1, "Inicio":null "Fin":null},
+		{"P1":12, "P2":5, "TR":1, "Inicio":null "Fin":null},
+		{"P1":13, "P2":4, "TR":1, "Inicio":null "Fin":null},
+		{"P1":14, "P2":4, "TR":1, "Inicio":null "Fin":null},
+		{"P1":11, "P2":12, "TR":2, "Inicio":null "Fin":null},
+		{"P1":13, "P2":14, "TR":2, "Inicio":null "Fin":null},
+		{"P1":16, "P2":14, "TR":1, "Inicio":null "Fin":null},
+		{"P1":17, "P2":14, "TR":1, "Inicio":null "Fin":null},
+		{"P1":16, "P2":17, "TR":2, "Inicio":null "Fin":null},
+		{"P1":18, "P2":13, "TR":1, "Inicio":null "Fin":null},
+		{"P1":19, "P2":13, "TR":1, "Inicio":null "Fin":null},
+		{"P1":18, "P2":19, "TR":2, "Inicio":null "Fin":null}
+
+	]
+}
+]}
