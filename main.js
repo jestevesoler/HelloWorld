505bdeb883b8bca318c22179093ce12cc1c94056
JMEsteve <jestevesoler@gmail.com>
2014-10-28 17:49:59 +0100

    Probando

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-$(document).ready(function (){
-    $("#btn382").click(function(){
-        /* set no cache */
-        $.ajaxSetup({ cache: false });
-        $.getJSON("/HelloWorld/Personas.json", function(data){
-            var html = [];
+function retornarPersonas()
+{
+  /* set no cache */
+  $.ajaxSetup({ cache: false });
+  $.getJSON("/HelloWorld/Personas.json", function(data){
+    var html = [];
 
-            /* loop through array */
-            $.each(data, function(index, d){
-                html.push("<b> Nombre : </b> ", d.Nombre, "<b> F.Nac : </b>" ,
-                            d.FNac , " <b> F.Def : </b> " , d.FDec ,"<br>");
-            });
-
-
-            $("#div381").html(html.join(''))
-        }).error(function(jqXHR, textStatus, errorThrown){ /* assign handler */
-            /* alert(jqXHR.responseText) */
-            alert("error occurred!");
-        });
+    /* loop through array */
+    $.each(data, function(index, d){
+      html.push("<li> <b> Nombre : </b> ", d.Nombre, "<b> F.Nac : </b>" ,
+                d.FNac , " <b> F.Def : </b> " , d.FDec ,"</li>");
     });
-});
+
+
+    $("#div381").html(html.join(''))
+  }).error(function(jqXHR, textStatus, errorThrown){ /* assign handler */
+    /* alert(jqXHR.responseText) */
+    alert("error occurred!");
+    });
+}
